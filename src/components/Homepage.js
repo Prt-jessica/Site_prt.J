5d9282fb5f213073f121946cd2ad7a7081a3ec3a
Jess <pirotte.jessica18@gmail.com>
2019-10-23 15:55:42 +0200

    Debug Bokeh(hors champ) Ajout Logo

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Bokeh from "./Bokeh";
+import Logo from "./Logo";
 
 const Homepage = () => {
   return (
     <div>
       <Bokeh />
+      <Logo />
     </div>
   );
 };
---
+++ src/components/Logo.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Logo = () => {
+  return (
+    <div className="logo">
+      <div id="border">
+        <p id="bordercontent">
+          <span id="name">Prt. J</span>
+        </p>
+      </div>
+    </div>
+  );
+};
+export default Logo;

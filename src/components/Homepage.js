d4ca1f76057a60ae537d3bf679203064cfbfac43
Jess <pirotte.jessica18@gmail.com>
2019-10-23 16:59:19 +0200

    Add Smoke

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Bokeh from "./Bokeh";
 import Logo from "./Logo";
+import Smoke from "./smoke";
 
 const Homepage = () => {
   return (
     <div>
       <Bokeh />
       <Logo />
+      <Smoke />
     </div>
   );
 };
---
+++ src/components/smoke.js
@@ -0,0 +1,7 @@
+import React from "react";
+// import Smoke from "../asset/Smoke.png";
+
+const smoke = () => {
+  return <div className="smoke"></div>;
+};
+export default smoke;

dfc365bdb44d3a60a7755342a59ae9b89bb3958e
Jess <pirotte.jessica18@gmail.com>
2019-11-05 09:18:51 +0100

    deploy

---
+++ build/precache-manifest.487f5dfc53076c2cdad3f255f75ec6c3.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f4e410bde6d70adecef704520f447c2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f0b72f6edc57f60c440f",
+    "url": "/static/css/main.4d162ab9.chunk.css"
+  },
+  {
+    "revision": "4c8649c8ef0e5bae0b24",
+    "url": "/static/js/2.8e0ed0c1.chunk.js"
+  },
+  {
+    "revision": "f0b72f6edc57f60c440f",
+    "url": "/static/js/main.5a361ea5.chunk.js"
+  },
+  {
+    "revision": "61eafababc593f4f0c00",
+    "url": "/static/js/runtime-main.6edd80a4.js"
+  },
+  {
+    "revision": "3f864d85d00e6a91a091df0f857f62d8",
+    "url": "/static/media/Abuget.3f864d85.ttf"
+  },
+  {
+    "revision": "29e7458e0ef5c2c55f1d2f4cd4e1b9a2",
+    "url": "/static/media/BookShelf.29e7458e.png"
+  },
+  {
+    "revision": "e9a0dc2a602997e66af2ac65e4c28070",
+    "url": "/static/media/SadwellRegular.e9a0dc2a.ttf"
+  },
+  {
+    "revision": "8bfe76edd9ec1657af65e0199243109f",
+    "url": "/static/media/simplicity.8bfe76ed.ttf"
+  },
+  {
+    "revision": "7b9628ba5e85815e4bb34b2769762b5f",
+    "url": "/static/media/smoke.7b9628ba.png"
+  }
+]);
